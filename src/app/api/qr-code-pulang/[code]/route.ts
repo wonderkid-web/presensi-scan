24b5fd8a3114e6081e47131cf3893a35fe855509
wonderkid-web <mwahyuap2018@gmail.com>
2024-03-26 16:09:45 +0700

    Fix find function on qr-code-pulang

--- src/app/api/qr-code-pulang/[code]/route.ts
+++ src/app/api/qr-code-pulang/[code]/route.ts
@@ -11,7 +11,11 @@ export async function GET(req: Request, { params }: { params: any }) {
     });
     const codes: Code[] = await raw.json();
 
-    const checkCode = codes.find((currCode) => currCode.code_masuk == code);
+    const checkCode = codes.find((currCode) => currCode.code_keluar == code);
+
+    console.log("checkcode", checkCode);
+    console.log("id", code);
+    console.log("codes", codes);
 
     if (checkCode) {
       await axios.post(`${process.env.NEXT_PUBLIC_BASE_API_URL}/user`, {
@@ -19,11 +23,14 @@ export async function GET(req: Request, { params }: { params: any }) {
         jam: new Date(),
         type: "pulang",
       });
-      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/proses/sukses`)
 
+      return NextResponse.redirect(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/proses/sukses`
+      );
     } else {
-      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/proses/gagal`)
-
+      return NextResponse.redirect(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/proses/gagal`
+      );
     }
   } catch (error) {
     return NextResponse.json({ error });
