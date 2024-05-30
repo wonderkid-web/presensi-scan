81b9cf5ce92d74346c00824be254391a40d41712
wonderkid-web <mwahyuap2018@gmail.com>
2024-05-30 19:49:47 +0700

    update hanya menampilkan QRcode terbaru saja

--- src/app/absensi/page.tsx
+++ src/app/absensi/page.tsx
@@ -1,29 +1,25 @@
+// @ts-nocheck
 "use client";
 
 import { createCode, getCode, getUser } from "@/actions";
-import useAuth from "@/hooks/useAuth";
 import { Code } from "@/types";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 import QRCode from "react-qr-code";
 import uuid from "react-uuid";
 
 export default function Home() {
   const queryClient = useQueryClient();
-  const { user: currentUser } = useAuth();
-  const router = useRouter();
 
-  const { data, isLoading } = useQuery<Code[]>({
+  const { data, isLoading } = useQuery<Code>({
     queryFn: getCode,
     queryKey: ["code"],
+    select: (data) => data.reduce((latest, current) => {
+        return new Date(latest.createdAt) > new Date(current.createdAt) ? latest : current;
+      }, data[0])
+    
   });
 
-  const { data: user, isLoading: isLoadingUser } = useQuery<Code[]>({
-    queryFn: getUser,
-    queryKey: ["user"],
-  });
 
   const { mutate, isPending } = useMutation({
     mutationFn: createCode,
@@ -51,27 +47,26 @@ export default function Home() {
       <div className="mt-4 grid grid-cols-1 md:grid-cols-2 place-items-center gap-4">
         <div className="flex gap-5 flex-col items-center">
           <h1 className="font-bold underline text-2xl">Absen Masuk</h1>
-          {data?.map((code) => (
-            <div key={code.code_keluar}>
+       
+            <div>
               <QRCode
                 key={uuid()}
                 size={200}
                 // value={`${process.env.NEXT_PUBLIC_BASE_URL}/api/qr-code-masuk/${code.code_masuk}`}
-                value={`${code.code_masuk}`}
+                value={`${data?.code_masuk}`}
               />
             </div>
-          ))}
         </div>
         <div className="flex gap-5 flex-col items-center">
           <h1 className="font-bold underline text-2xl">Absen Keluar</h1>
-          {data?.map((code) => (
+
             <QRCode
               key={uuid()}
               size={200}
               // value={`${process.env.NEXT_PUBLIC_BASE_URL}/api/qr-code-pulang/${code.code_keluar}`}
-              value={`${code.code_keluar}`}
+              value={`${data?.code_keluar}`}
             />
-          ))}
+          
         </div>
       </div>
       {/* <div className="mt-4 grid grid-cols-1 md:grid-cols-2 place-items-center gap-4">
